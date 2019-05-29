3ecc35781f5375872c1043a28ae23ce3a4db9fff
Tammy Vo <votammy97@gmail.com>
2019-05-28 17:11:46 -0700

    Updates.

--- DIYapplication/src/model/Materials.java
+++ DIYapplication/src/model/Materials.java
@@ -8,32 +8,32 @@ import java.util.HashMap;
  * @author Tammy Vo
  */
 public class Materials {
-	
+
 	/** The HashMap storing all materials needed. */
 	private HashMap<String, Double> myMaterials;
-	
+
 	/** Private constructor. */
-	public Materials(){
+	public Materials() {
 		myMaterials = new HashMap<>();
 	}
-	
+
 	/**
-	 * Adds the material. 
+	 * Adds the material.
 	 * 
-	 * @param theName of the material. 
-	 * @param theCost of the material. 
+	 * @param theName of the material.
+	 * @param theCost of the material.
 	 */
 	public void addMaterials(final String theName, final double theCost) {
 		myMaterials.put(theName, theCost);
 	}
-	
+
 	/**
-	 * Returns the materials. 
+	 * Returns the materials.
 	 * 
-	 * @return the materials. 
+	 * @return the materials.
 	 */
-	public HashMap<String, Double> getMaterialMap(){
+	public HashMap<String, Double> getMaterialMap() {
 		return myMaterials;
 	}
-	
+
 }
--- DIYapplication/src/model/ProjectList.java
+++ DIYapplication/src/model/ProjectList.java
@@ -8,27 +8,27 @@ import java.util.ArrayList;
  * @author Tammy Vo
  */
 public class ProjectList {
-	
+
 	/** ArrayList to store the projects. */
-	private ArrayList<Project> myProjectList; 
+	private ArrayList<Project> myProjectList;
 
 	/** Constructor for ProjectList. */
 	public ProjectList() {
 		myProjectList = new ArrayList<>();
 	}
-	
+
 	/** Method to add projects to project list. */
 	public void addProject(final Project theProject) {
 		myProjectList.add(theProject);
 	}
-	
+
 	/** Method to delete projects from project list. */
 	public void deleteProject(final Project theProject) {
 		myProjectList.remove(theProject);
 	}
-	
+
 	/** Method to get project list. */
-	public ArrayList<Project> getProjectList(){
+	public ArrayList<Project> getProjectList() {
 		return myProjectList;
 	}
 
